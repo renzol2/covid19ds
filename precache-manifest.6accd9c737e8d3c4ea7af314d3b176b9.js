148d04ae679584041412046de7c9767cd5dda4d5
Renzo Ledesma <renzol2@illinois.edu>
2020-05-20 16:27:33 -0500

    Updates

---
+++ precache-manifest.6accd9c737e8d3c4ea7af314d3b176b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b377b44535843f0525d72394bf0d29",
+    "url": "/renzomledesma/index.html"
+  },
+  {
+    "revision": "feba396809374264df7f",
+    "url": "/renzomledesma/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bbd5f527bfcadd9dd4f",
+    "url": "/renzomledesma/static/js/2.dd254eac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/renzomledesma/static/js/2.dd254eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feba396809374264df7f",
+    "url": "/renzomledesma/static/js/main.e5eeb2a9.chunk.js"
+  },
+  {
+    "revision": "df7df30601898ed290c7",
+    "url": "/renzomledesma/static/js/runtime-main.9115dc67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/renzomledesma/static/media/logo.5d5d9eef.svg"
+  }
+]);
