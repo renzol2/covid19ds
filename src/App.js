d02b0e86823f8b1dc38c2120ffff34055b3b56c7
Renzo Ledesma <renzol2@illinois.edu>
2020-05-21 17:58:43 -0500

    install tone.js and try their hello tone example

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import * as Tone from 'tone';
 
-const MediaCard = ({title, body, imageUrl}) => {
+const playTone = () => {
+  const synth = new Tone.Synth().toMaster();
+  synth.triggerAttackRelease('C4', '8n');
+}
+
+const App = () => {
   return (
     <div>
-      <h2>{title}</h2>
-      <p>{body}</p>
-      <img src={imageUrl} alt="test" />
+      hey. my name is renzo. i'm still learning so i don't have much here but i hope you have a great day!
+      <br />
+      <button onClick={() => {playTone()}}>
+        play a c4
+      </button>
     </div>
-  );
-}
-
-const Hi = ({firstName, lastName}) => {
-  return (
-    <div className="Main">
-      <h1>This is a test.</h1>
-      <h2>I don't remember HTML tags lmao</h2>
-      <p>Saying hello to {firstName} {lastName}.</p>
-    </div>
-  );
-}
-
-const Gate = ({isOpen}) => {
-  const display = isOpen ? 'open' : 'closed';
-  return (
-    <p>{display}</p>
   )
 }
 
-const paragraph = 'this is my paragraph :)';
-const bodyText = 'this is the body text! uwu';
-
-const Everything = () => {
-  return (
-    <>
-      <MediaCard
-        title={paragraph}
-        body={<strong>{bodyText}</strong>}
-        imageUrl="https://m.media-amazon.com/images/I/4141ztFVb3L._SS500_.jpg"
-      />
-      <Hi
-        firstName="hi"
-        lastName="bye"
-      />
-      <Gate isOpen={true} />
-    </>
-  )
-}
-
-export default Everything;
+export default App;
