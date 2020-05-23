2c6c9b969dcf0b034c29075c835ed10c5815be58
Renzo Ledesma <renzol2@illinois.edu>
2020-05-23 14:16:56 -0500

    add axios dependency and learn how to use

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React, {useState} from 'react';
 import './App.css';
 import * as Tone from 'tone';
+import axios from 'axios';
 
 const playTone = () => {
   const synth = new Tone.Synth().toMaster();
   synth.triggerAttackRelease('C4', '8n');
 }
 
+const Reddit = () => {
+  const [posts, setPosts] = useState([]);
+
+  // API call using axios
+  React.useEffect(() => {
+    axios.get('https://www.reddit.com/r/reactjs.json')
+      .then(res => {
+        const newPosts = res.data.data.children
+          .map(obj => obj.data);
+
+        setPosts(newPosts);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>r/reactjs</h1>
+
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
+
 const App = () => {
   return (
     <div>
-      hey. my name is renzo. i'm still learnin' so i don't have much here but i hope you have a great day!
-      <br />
-      <button onClick={() => {playTone()}}>
-        play a c4
-      </button>
+      <Reddit />
     </div>
   )
 }
