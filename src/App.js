b01e72b9d0b25acb645a020bc3995535ce1cd266
Renzo <renzol2@illinois.edu>
2020-05-25 23:51:30 -0500

    allow oscillator type selections by user

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import * as Tone from 'tone';
 
 import fetchData from './data/fetchData';
 
-const playTone = midiPitch => {
-  var synth = new Tone.Synth().toMaster();
+const playTone = (midiPitch, oscType) => {
+  const options = {oscillator: {
+    type: oscType,
+  }};
+  var synth = new Tone.Synth(options).toMaster();
   synth.triggerAttackRelease(Tone.Frequency(midiPitch, 'midi'), '8n');
 }
 
 const App = () => {
   const defaultPitch = 60;
+  const defaultOscSelection = 1;
+  const oscTypes = [
+    'sine',
+    'triangle',
+    'square',
+    'sawtooth',
+  ];
+
   const [data, setData] = useState([]);
   const [pitch, setPitch] = useState(defaultPitch);
+  const [oscSelection, setOscSelection] = useState(defaultOscSelection);
+
   const amount = 5;
   let key = 0;
   
@@ -34,11 +47,18 @@ const App = () => {
       </ul> */}
 
       { /* tone js stuff */}
-      <p>The current MIDI pitch is {pitch}</p>
-      <button onClick={() =>playTone(pitch)}>play</button>
+      <p>The current MIDI pitch is: {pitch}</p>
+      <p>The current oscillator is: {oscTypes[oscSelection]}</p>
+      <button onClick={() =>playTone(pitch, oscTypes[oscSelection])}>play</button>
       <br />
-      <button onClick={() =>setPitch(pitch + 1)}>Increase pitch</button>
-      <button onClick={() =>setPitch(pitch - 1)}>Decrease pitch</button>
+      <button onClick={() => setPitch(pitch + 1)}>Increase pitch</button>
+      <button onClick={() => setPitch(pitch - 1)}>Decrease pitch</button>
+      <br />
+      <button onClick={
+          () => setOscSelection((oscSelection + 1) % oscTypes.length)
+        }>
+          Toggle oscillator
+      </button>
     </div>
   )
 }
