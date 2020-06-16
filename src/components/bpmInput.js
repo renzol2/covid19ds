69b9116ed97f3b5bd8d0f8b063c48aa75009304e
Renzo <renzol2@illinois.edu>
2020-06-16 00:00:27 -0500

    create bpm input component file

---
+++ src/components/bpmInput.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+
+class BpmInput extends Component {
+  render() {
+    return (
+      <div>
+        <InputGroup>
+          <InputGroup.Prepend>
+            <InputGroup.Text>Playback BPM</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder='Ex: 200'
+            aria-label='BPM'
+            onChange={
+              (event) => this.handleInput(event, this.setBpm)
+            }
+          />
+        </InputGroup>
+        <p>
+          Current BPM: <strong>{this.props.bpm}</strong>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default BpmInput;
