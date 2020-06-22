c9b20cd4b5455d8a863c5839d70d51ca050a8114
Renzo <renzol2@illinois.edu>
2020-06-21 23:51:53 -0500

    create file for quantize func

---
+++ src/util/quantizeNote.js
@@ -0,0 +1,11 @@
+/**
+ * Quantizes a MIDI note based on the given scale
+ * @param {number} note MIDI note to be quantized
+ * @param {Array} scale list of notes in scale
+ * @returns {number} quantized MIDI note
+ */
+function quantizeNote(note, scale) {
+  return 0;
+}
+
+export default quantizeNote;
